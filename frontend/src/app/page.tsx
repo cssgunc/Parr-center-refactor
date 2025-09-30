8441fb78ec8051757a9444df5869522bfcc2caac
Jordweinstein <jordanweinstein556@gmail.com>
2025-09-29 21:56:28 -0400

    connect firebase integration to next.js

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,50 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useGetDocuments } from "@/hooks/useGetDocuments";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  const { getDoc } = useGetDocuments();
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const data = await getDoc("users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+      <main>
+        <h1>Users</h1>
+        {users.length > 0 ? (
+          <ul>
+            {users[0].name} - {users[0].email}
+          </ul>
+        ) : (
+          <p>Loading users...</p>
+        )}
       </main>
       <footer className={styles.footer}>
         <a
---
+++ frontend/src/hooks/useGetDocuments.ts
@@ -0,0 +1,20 @@
+import { db } from "../lib/firebase/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+
+export const useGetDocuments = () => {
+
+  const getDoc = async (path: string) => {
+
+    const collectionRef = collection(db, path)
+    const querySnapshot = await getDocs(collectionRef)
+    const data = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    return data;
+  }
+
+  return { getDoc }
+}
