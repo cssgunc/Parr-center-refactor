002d52de5ce698e77bf7745d7fbabe04b535c03e
Ivan Wu <ivanwu@unc.edu>
2025-10-09 18:27:30 -0400

    Add functions for Module and Step CRUD

--- frontend/src/lib/firebase/db-operations.ts
+++ frontend/src/lib/firebase/db-operations.ts
@@ -1,5 +1,15 @@
 import { db } from "./firebaseConfig";
-import { collection, doc, getDoc, setDoc, addDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+} from "firebase/firestore";
 
 const usersCollection = collection(db, "users");
 const modulesCollection = collection(db, "modules");
@@ -7,6 +17,8 @@ const stepsCollection = collection(db, "steps");
 const userProgressCollection = collection(db, "progress");
 
 // Module CRUD operations
+
+// Get module by ID - DOES NOT include steps subcollection
 const getModulebyId = async (moduleId: string) => {
   const moduleDocRef = doc(modulesCollection, moduleId);
   const moduleDoc = await getDoc(moduleDocRef);
@@ -28,8 +40,47 @@ const updateModule = async (moduleId: string, moduleData: any) => {
   return { id: moduleId, ...moduleData };
 };
 
-// Step CRUD operations
+const deleteModule = async (moduleId: string) => {
+  const moduleDocRef = doc(modulesCollection, moduleId);
+  await deleteDoc(moduleDocRef);
+  // TODO: Does not delete subcollections (steps), need to consider how to implement that.
+};
 
-// User progress operations
+// Step CRUD operations
+const getStepById = async (stepId: string) => {
+  const stepDocRef = doc(stepsCollection, stepId);
+  const stepDoc = await getDoc(stepDocRef);
+  if (stepDoc.exists()) {
+    return { id: stepDoc.id, ...stepDoc.data() };
+  } else {
+    throw new Error("Step not found");
+  }
+};
+
+// Get steps by module ID
+const getStepByModuleId = async (moduleId: string) => {
+  const stepsQuery = query(stepsCollection, where("moduleId", "==", moduleId));
+  const stepsSnapshot = await getDocs(stepsQuery);
+  stepsSnapshot.forEach((doc) => {
+    console.log({ id: doc.id, ...doc.data() });
+  });
+  return stepsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+};
+
+const createStep = async (stepData: any) => {
+  const stepDocRef = await addDoc(stepsCollection, stepData);
+  return { id: stepDocRef.id, ...stepData };
+};
+
+const updateStep = async (stepId: string, stepData: any) => {
+  const stepDocRef = doc(stepsCollection, stepId);
+  await setDoc(stepDocRef, stepData, { merge: true });
+  return { id: stepId, ...stepData };
+};
+
+const deleteStep = async (stepId: string) => {
+  const stepDocRef = doc(stepsCollection, stepId);
+  await deleteDoc(stepDocRef);
+};
 
 // Query helpers (getPublicModules, getUserModules, etc.)
