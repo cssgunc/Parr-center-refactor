0ac4c3710bb95bda7aa41d86d0cceca2a50cbd63
Krushna-B <Krushna.Bhanushali@unc.edu>
2025-10-11 17:18:56 -0400

    Add function for updating user password

--- frontend/src/lib/firebase/Authentication/EmailAuth/index.ts
+++ frontend/src/lib/firebase/Authentication/EmailAuth/index.ts
@@ -1,31 +1,115 @@
-import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
-import {auth} from "@/lib/firebase/firebaseConfig"
+import {
+  createUserWithEmailAndPassword,
+  reauthenticateWithCredential,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  updatePassword,
+} from "firebase/auth";
+import { auth } from "@/lib/firebase/firebaseConfig";
+import { EmailAuthProvider } from "firebase/auth/web-extension";
 
-
-export const registerUser = async (name: string, email: string, password: string) => {
-    try{
+/**
+ *Register's user accounts in firebase auth
+ *
+ * @param name - User's Name
+ * @param email - User's Email
+ * @param password - User's Password
+ */
+export const registerUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  try {
     console.log(name);
     console.log(email);
     console.log(password);
 
     //Create User
     const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-    )
-    const results = userCredential.user
-    console.log(results)
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log(user);
 
     //Send verifcation email to the user
-    await sendEmailVerification(results)
+    await sendEmailVerification(user);
     alert(`A verifcation email has been sent to your email address ${name}`);
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+};
 
-    } catch (error){
-        console.log(error) 
-
-    } finally{
-
+/**
+ *Signs user into their accounts using their email and password
+ *
+ * @param email - User's Email
+ * @param password - User's Password
+ * @returns -  Returns on unverified email
+ */
+export const loginUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  try {
+    //Login User
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    if (user.emailVerified === false) {
+      alert("Please verify your email to login");
+      return;
     }
-   
-}
+
+    console.log(email, password);
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+};
+
+export const updateUserPassword = async (
+  currentPassword: string,
+  newPassword: string
+) => {
+  try {
+    //Check valid user
+    const user = auth.currentUser;
+    if (!user) {
+      return;
+    }
+
+    //check if old password input is valid
+    if (!currentPassword || currentPassword === "") {
+      alert("Please enter your current password");
+      return;
+    }
+    //check if new password input is valid
+    if (!newPassword || newPassword === "") {
+      alert("Please enter your new password");
+      return;
+    }
+
+    //validate old password
+    const authCredential = EmailAuthProvider.credential(
+      user.email as string,
+      currentPassword
+    );
+
+    //Reauthenticate user
+    await reauthenticateWithCredential(user, authCredential);
+    //update password
+    await updatePassword(user, newPassword);
+    alert("Password Updated Sucessfully");
+
+    //Navigation
+  } catch (error) {
+    console.log(error);
+  }
+};
