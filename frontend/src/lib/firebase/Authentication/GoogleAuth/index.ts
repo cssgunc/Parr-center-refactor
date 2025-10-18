7a0a9c81c7185b745835b3eef55f4deea642d7f1
Krushna-B <Krushna.Bhanushali@unc.edu>
2025-10-17 22:05:58 -0400

    Add login form

--- frontend/src/lib/firebase/Authentication/GoogleAuth/index.ts
+++ frontend/src/lib/firebase/Authentication/GoogleAuth/index.ts
@@ -2,6 +2,7 @@ import { FirebaseError } from "firebase/app";
 import { generateFirebaseAuthErrorMessage } from "../ErrorHandler";
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleAuthProvider } from "../../firebaseConfig";
+import { toast } from "sonner";
 
 export const signInUserWithGoogleAuth = async () => {
   try {
@@ -10,7 +11,7 @@ export const signInUserWithGoogleAuth = async () => {
       throw new Error("No user found");
     }
     const user = result.user;
-    alert(`Welcome ${user.displayName}`);
+    toast.success(`Welcome ${user.displayName}`);
     //Navigate to Page
   } catch (error) {
     if (error instanceof FirebaseError) {
