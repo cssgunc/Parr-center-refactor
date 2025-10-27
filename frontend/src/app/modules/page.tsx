e74524a3554246f2a4f088151da7df47d0228e8b
Eric Li <ericlmtn@gmail.com>
2025-10-27 15:33:57 -0400

    Create testing page for fetching module by id

--- frontend/src/app/modules/page.tsx
+++ frontend/src/app/modules/page.tsx
@@ -1,18 +1,43 @@
 'use client';
 
-import { Container, Typography, Box } from '@mui/material';
+import { getModuleById, getUserModules } from '@/lib/firebase/db-operations';
+import { Module } from '@/lib/firebase/types';
+import { Container, Typography, Box, Input } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function ModulesPage() {
-  return (
-    <Container maxWidth="lg">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h3" component="h1" gutterBottom>
-          Modules
-        </Typography>
-        <Typography variant="body1">
-          This is the modules page. Add your modules content here.
-        </Typography>
-      </Box>
-    </Container>
-  );
+	const [id, setId] = useState<string>('DY0C6mviJBcQDoFfbMPx');
+	const [module, setModule] = useState<Module | null>(null);
+
+	useEffect(() => {
+		if (id.length == 'DY0C6mviJBcQDoFfbMPx'.length) {
+			getModuleById(id)
+				.then((module) => {
+					console.log('Module fetched by ID:', module);
+					setModule(module);
+				})
+				.catch((error) => {
+					console.error('Error fetching module by ID:', error);
+				});
+		}
+	}, [id]);
+
+	return (
+		<Container maxWidth="lg">
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h3" component="h1" gutterBottom>
+					Modules
+				</Typography>
+				<Input value={id} onChange={(e) => setId(e.target.value)} />
+        {module && (
+          <Box sx={{ mt: 2 }}>
+            <Typography variant="h5">Module Details:</Typography>
+            <Typography>ID: {module.id}</Typography>
+            <Typography>Title: {module.title}</Typography>
+            <Typography>Description: {module.description}</Typography>
+          </Box>
+        )}
+			</Box>
+		</Container>
+	);
 }
